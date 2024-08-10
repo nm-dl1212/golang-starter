6205e01e90663232d9be7baef515dd99af00f1d1
nmatsu <n.matsumura2022@gmail.com>
2024-08-10 15:47:16 +0000

    mod: tasks

--- main.go
+++ main.go
@@ -10,10 +10,16 @@ import (
 
 func main() {
 	db := db.NewDB()
+
 	userRepository := repository.NewUserRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+
+	taskRepository := repository.NewTaskRepository(db)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
+	taskController := controller.NewTaskController(taskUsecase)
+
+	e := router.NewRouter(userController, taskController)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
